e4d24066492172f6f897e949e8b9b42ceb9d1e81
Roberto Rojo <rrojo.va@gmail.com>
2022-11-13 18:53:24 +0100

    Initial Commit

---
+++ MarvelSwiftUICombine/MarvelSwiftUICombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelSwiftUICombineApp.swift
+//  MarvelSwiftUICombine
+//
+//  Created by Roberto Rojo Sahuquillo on 13/11/22.
+//
+
+import SwiftUI
+
+@main
+struct MarvelSwiftUICombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
